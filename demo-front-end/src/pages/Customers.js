7ac1b4630b6cab474b566e3c73b6fc06d0664745
telara <taraelsen@gmail.com>
2024-11-14 23:11:19 +0100

    display list of customers in front-end app

--- demo-front-end/src/pages/Customers.js
+++ demo-front-end/src/pages/Customers.js
@@ -1,40 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Container, Paper, Table, TableBody, TableCell, 
     TableContainer, TableHead, TableRow } from '@mui/material';
 
 function Customers() {
-  const paperStyle={padding:20, height:'80vh', width:'70wh', margin:"20px auto"}
-  const headerStyle={color:'burlywood', "justify-self": "center"}
+  const [customers, setCustomers] = useState([]);
+  const paperStyle={padding:20, height:'80vh', width:'70vw', margin:"20px auto"}
+  const headerStyle = { color: 'burlywood', textAlign: "center", padding:20 };
+
+  // Fetch customers from backend
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/customers')
+      .then(response => {
+        setCustomers(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the customer data!", error);
+      });
+  }, []);
 
   return (
     <Container>
-        <Paper elevation={3} style={paperStyle}>
+      <Paper elevation={3} style={paperStyle}>
         <h2 style={headerStyle}>List of Customers</h2>
-        <div>
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Customer Name</TableCell>
-                            <TableCell>Account</TableCell>
-                            <TableCell>Credit</TableCell>
-                            <TableCell>Date of Opening</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {/* {PostData.map((list, index) => (
-                        <TableRow key={index}>
-                            <TableCell>{list.first_name}</TableCell>
-                            <TableCell>{list.email}</TableCell>
-                        </TableRow>
-                      ))} */}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-        </Paper>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Customer ID</TableCell>
+                <TableCell>Customer Name</TableCell>
+                <TableCell>Account</TableCell>
+                <TableCell>Credit</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {customers.map((customer, index) => (
+                <TableRow key={index}>
+                  <TableCell>{customer.id}</TableCell>
+                  <TableCell>{customer.name} {customer.surname}</TableCell>
+                  <TableCell>{customer.account ? 'Yes' : null}</TableCell>
+                  <TableCell>{customer.account? customer.balance : null}</TableCell>
+                  {/* <TableCell>{customer.account && customer.account.transactions && 
+                    customer.account.transactions.length > 0 ? customer.account.transactions[0].datetime.slice(0, 10) : null}</TableCell> */}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </Container>
-    )
+  );
 }
 
 export default Customers;
